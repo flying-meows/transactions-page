cbd9525a4d7d5ddf539e285908f833535d24e351
sawthinkar <10482589+sawthinkar@users.noreply.github.com>
2021-01-06 15:56:50 +0630

    add profile image and backgound color

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:transactions_page/sceens/TransactionsScreen.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,21 +10,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
+        ),
+        home: Scaffold(
+          backgroundColor: Color.fromRGBO(39, 50, 80, 1),
+          body: TransactionsScreen(),
+        ));
   }
 }
 
---
+++ lib/sceens/TransactionsScreen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class TransactionsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        SizedBox(
+          width: 80,
+          height: 80,
+          child: Image.asset("images/profile.png"),
+        )
+      ],
+    );
+  }
+}
