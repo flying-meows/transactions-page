69358425da806b8a0ba5ed3c25a6414b60681a22
sawthinkar <10482589+sawthinkar@users.noreply.github.com>
2021-01-06 22:39:51 +0630

    add ListView  and Title Text with Padding

--- lib/sceens/TransactionsScreen.dart
+++ lib/sceens/TransactionsScreen.dart
@@ -5,6 +5,7 @@ class TransactionsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
+        // Profile picture and username in the child Row
         Padding(
           // Padding on the left of the profile image
           padding: EdgeInsets.only(left: 20, top: 10),
@@ -38,10 +39,10 @@ class TransactionsScreen extends StatelessWidget {
             ],
           ),
         ),
+        // CardView below profile image and username
         Padding(
             padding: EdgeInsets.only(left: 20, top: 10, right: 20, bottom: 10),
             child: Container(
-                // Block under profile image and username
                 height: 200,
                 decoration: BoxDecoration(
                     gradient: LinearGradient(
@@ -62,7 +63,47 @@ class TransactionsScreen extends StatelessWidget {
                       ),
                     ],
                   ),
-                )))
+                ))),
+        // Title of Transactions ListView (Text with padding)
+        Padding(
+          padding: EdgeInsets.only(left: 20, top: 10, right: 20, bottom: 20),
+          child: Text(
+            "ALL TRANSACTIONS",
+            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+          ),
+        ),
+        // Title of Transactions ListView (Text without padding)
+        // Text(
+        //   "ALL TRANSACTIONS",
+        //   style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+        // ),
+
+        // Transactions ListView
+        Expanded(
+          child: ListView.builder(
+            itemBuilder: (BuildContext context, int index) {
+              return Padding(
+                padding: EdgeInsets.only(left: 20, right: 20, top: 10),
+                child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                      color: Color.fromRGBO(55, 65, 104, 1)),
+                  child: ListTile(
+                    title: Text(
+                      "Transaction $index",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.white),
+                    ),
+                    subtitle: Text(
+                      "Some brief detail of transaction no $index",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+        )
       ],
     );
   }
